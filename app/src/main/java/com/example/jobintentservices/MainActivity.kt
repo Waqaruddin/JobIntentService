81be6cc52711aa2834b083430455adee7c954ba5
Syed Waqar Uddin <uddin.8@wright.edu>
2020-10-21 13:54:16 -0400

    Destroy service

--- app/src/main/java/com/example/jobintentservices/MainActivity.kt
+++ app/src/main/java/com/example/jobintentservices/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -12,12 +13,18 @@ class MainActivity : AppCompatActivity() {
         val intent = Intent(this , MyJobIntentService::class.java )
         //val intent = Intent(this , NormalService::class.java )
 
+        // for calling onHandleWork on different thread and not on main thread
+        MyJobIntentService.enqueueWork(this , intent)
         startService(intent)
 
-        MyJobIntentService.enqueueWork(this , intent)
     }
 
     override fun onDestroy() {
         super.onDestroy()
+
+        //to destroy
+//        val intent = Intent(this , MyJobIntentService::class.java )
+//
+//        stopService(intent)
     }
 }
